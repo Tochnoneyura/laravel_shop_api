c053b520806e1eb6774eff53ce8c7ff391e5014a
Tochnoneyura <tochnoneyura@gmail.com>
2023-08-08 21:03:18 +0400

    delete users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\UpdateUserRequest;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\UpdateUserRequest;
 
 class UserController extends Controller
 {
@@ -53,24 +54,24 @@ class UserController extends Controller
 
         switch(true) {
 
-            case(($id !== $currentUser['id']) AND ($currentUser['role'] !== 'admin')):
+            case(((int) $id !== $currentUser['id']) AND ($currentUser['role'] !== 'admin')):
                 return response()->json(['error' => 'Admin check failed'], 401);
             
-            case(($id === $currentUser['id']) AND ($currentUser['role'] === 'customer')):
-                    $user->password =  Hash::make($data['password']);
-                    $user->name = $data['name'];
-                    $user->last_name = $data['last_name'];
-                    $user->second_name = $data['second_name'];
+            case(((int) $id === $currentUser['id']) AND ($currentUser['role'] === 'customer')):
+                    $user->password =  Hash::make($data['password']) ?? $user->password;
+                    $user->name = $data['name']?? $user->name;
+                    $user->last_name = $data['last_name'] ?? $user->last_name;
+                    $user->second_name = $data['second_name'] ?? $user->second_name;
                     $user->save();
 
                 return response(['message' => 'ok', 200]);
 
             case($currentUser['role'] === 'admin'):
-                    $user->password =  Hash::make($data['password']);
-                    $user->name = $data['name'];
-                    $user->last_name = $data['last_name'];
-                    $user->second_name = $data['second_name'];
-                    $user->role = $data['role'];
+                    $user->password =  Hash::make($data['password']) ?? $user->password;
+                    $user->name = $data['name'] ?? $user->name;
+                    $user->last_name = $data['last_name'] ?? $user->last_name;
+                    $user->second_name = $data['second_name'] ?? $user->second_name;
+                    $user->role = $data['role'] ?? $user->role;
                     $user->save();
                 return response(['message' => 'ok, admin', 200]);
 
@@ -79,6 +80,45 @@ class UserController extends Controller
 
     public function delete($id)
     {
-        
+        $currentUser =Auth::user();
+        $num = DB::table('users')->where('role', '=', 'admin')->count();
+        $user = User::findOrFail($id);
+
+        switch(true) {
+
+            case(((int) $id !== $currentUser['id']) AND ($currentUser['role'] !== 'admin')):
+                return response()->json(['error' => 'Admin check failed'], 401);
+            
+            case(((int) $id === $currentUser['id']) AND ($currentUser['role'] === 'customer')):
+    
+                DB::transaction(function () use($user){
+                    $user->active = 'N';
+                    $user->delete();
+                });
+                return response(['message' => 'ok', 200]);
+
+
+            case(((int) $id !== $currentUser['id']) AND ($currentUser['role'] === 'admin')):
+                
+                DB::transaction(function () use($user) {
+                    $user->active = 'N';
+                    $user->save();
+                    $user->delete();
+                });    
+                return response(['message' => 'ok, admin', 200]);
+            
+            
+            case(((int) $id === $currentUser['id']) AND ($currentUser['role'] === 'admin') AND ($num > 1)):
+
+                DB::transaction(function () use($user) {
+                    $user->active = 'N';
+                    $user->save();
+                    $user->delete();
+                });
+                return response(['message' => 'ok, admin', 200]);
+
+            case(((int) $id === $currentUser['id']) AND ($currentUser['role'] === 'admin') AND ($num <= 1)):
+                return response(['message' => 'You are the only admin', 401]);    
+        }
     }
 }
