4d843c0cb8cadef259736e67e9dc9a93cea26eec
Tochnoneyura <tochnoneyura@gmail.com>
2023-08-10 16:53:17 +0400

    hotfix

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 use Facade\Ignition\QueryRecorder\Query;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
